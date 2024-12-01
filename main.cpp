f8eff6ee99691cc93592b2f31ef27a0a4400600d
Yatofon <namesurnamecom76@gmail.com>
2024-12-01 23:14:31 +0200

    03_Lab_done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	cout << "Hello, 1 FIIT and 1 MO!" << endl;
-	return 0;
+// Лабораторная работа №3
+#include <cmath>  
+#include <iostream>  
+#include <iomanip>  
+ 
+using namespace std; 
+  
+int main(void) 
+{ 
+    unsigned long long x, i; 
+    cout << "Введите x, затем i" << endl; 
+    cin >> x >> i; 
+    cout << (( x & ( 1 << ( i - 1 ) ) ) >> ( i - 1) ); 
 }
