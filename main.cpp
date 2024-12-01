4fab7702c582c2474404a7df719dfad4931a6216
Yatofon <namesurnamecom76@gmail.com>
2024-12-01 23:11:28 +0200

    02_Lab_done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	cout << "Hello, 1 FIIT and 1 MO!" << endl;
-	return 0;
+//Лабораторная работа №2
+#include <cmath> 
+#include <iostream> 
+#include <iomanip> 
+ 
+using namespace std; 
+ 
+int main(void) { 
+    double S, h;
+	cin >> S >> h; 
+    double D = ( S * h ) / 3; 
+    cout << D; 
 }
