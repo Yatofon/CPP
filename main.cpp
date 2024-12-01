6f7473a0e7c70cf9d07904c1dcaa5645a3b0a3cf
Yatofon <namesurnamecom76@gmail.com>
2024-12-01 23:17:50 +0200

    04_Lab_done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,63 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
+// Лабораторная работа № 4
+#include <cmath>  
+#include <iostream>  
+#include <iomanip>  
+ 
+using namespace std; 
+ 
+void Task_1()
 {
-	cout << "Hello, 1 FIIT and 1 MO!" << endl;
-	return 0;
+	long long a, b, c; 
+    cin >> a >> b >> c; 
+    if (a < b && b > c) cout << b + a - c; 
+    else if (b % c == 0) cout <<b / c - b; 
+    else cout << a * b - c; 
 }
+void Task_2()
+{
+    int N; 
+    cin >> N; 
+    if (N <= 12 && N >= 1) switch(N) 
+    { 
+        case 1: 
+            cout << "January"; break; 
+        case 2: 
+            cout << "February"; break; 
+        case 3: 
+            cout << "March"; break; 
+        case 4: 
+            cout << "April"; break; 
+        case 5: 
+            cout << "May"; break; 
+        case 6: 
+            cout << "June"; break; 
+        case 7: 
+            cout << "July"; break; 
+        case 8: 
+            cout << "August"; break; 
+        case 9: 
+            cout << "September"; break; 
+        case 10: 
+            cout << "October"; break; 
+        case 11: 
+            cout << "November"; break; 
+        case 12: 
+            cout << "December"; break; 
+        default: 
+            break; 
+    } 
+    else cout << "Число находится вне рабочего диапазона"; 
+}
+void Task_3()
+{
+	short x; 
+    cin >> x; 
+    if (x == -1) cout << "Negative number"; else cout << "Positive number"; 
+}
+int main(void) 
+{ 
+    Task_1;
+	Task_2;
+	Task_3;
+}
+
